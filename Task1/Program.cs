6b65604f7e926dff7988687388f5252119774202
Yakov Putiliov <jack27@ya.ru>
2023-12-07 22:36:59 +0300

    Complite task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,26 @@
 ﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
 
+char [,] stringToLine = {{'H','a','l','l','o',' '},{'w','o','r','l','d','!'}};
+
+string StringFromChar(char [,] chars){
+    string res = "";
+    foreach (char elem in chars){
+        res += elem;
+    }
+    return res;
+    
+}
+
+
+
+void Show2dChar(char [,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j=0; j<array.GetLength(1); j++){
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Show2dChar(stringToLine);
+Console.WriteLine(StringFromChar(stringToLine));
