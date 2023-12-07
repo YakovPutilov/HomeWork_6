f9222f48d7f6ffadcb928ce99a23f0df030495cb
Yakov Putiliov <jack27@ya.ru>
2023-12-08 00:14:03 +0300

    Complite task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,3 +2,26 @@
 //Сформировать строку, в которой слова расположены в обратном порядке. 
 //В полученной строке слова должны быть также разделены пробелами.
 
+string str = "Well, well, well mister Freeman";
+
+string GetReversWordPosition(string str){
+    string res = "";
+    char space = ' ';
+    int lastSpasePos = str.Length;
+    for (int i = 0; i < str.Length; i++){
+        if (str[str.Length-i-1] == space){
+            for (int j = 0; str.Length-i + j < lastSpasePos; j++){
+                res += str[str.Length-i+j];
+            }
+            res += " ";
+            lastSpasePos = str.Length-i-1;
+        }     
+   }
+   if (str[0] != space){
+            for (int i=0; i < lastSpasePos; i++){
+                res += str[i];
+            }
+        }
+    return res;
+}
+Console.WriteLine(GetReversWordPosition(str));
