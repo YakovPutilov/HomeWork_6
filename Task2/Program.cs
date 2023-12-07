dd21c405bcf0e34c1c0f9b7bea8a07a322d2a809
Yakov Putiliov <jack27@ya.ru>
2023-12-07 22:51:31 +0300

    Complite task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,3 +1,9 @@
 ﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
 //Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
 
+string newstring = "AhAbRa CaDaHaBrA";
+string Lowstring(string str){
+    return str.ToLower();
+} 
+
+Console.WriteLine(Lowstring(newstring));
